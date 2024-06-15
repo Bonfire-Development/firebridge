d42344a42a3ebc24a43480cbaaa9d753dd49b416
EricApostal <ehapostal04@gmail.com>
2024-06-15 17:28:24 -0400

    added some of the base code for member list groups

---
+++ lib/src/models/guild/member_list_group.dart
@@ -0,0 +1,21 @@
+// import 'package:firebridge/firebridge.dart';
+// import 'package:firebridge/src/http/managers/member_list_group_manager.dart';
+
+// /// {@template martial_member_list_group}
+// /// A partial [MemberListGroup] object.
+// /// {@endtemplate}
+// class PartialMemberListGroupManager
+//     extends WritableSnowflakeEntity<MemberListGroup> {
+//   @override
+//   final MemberListGroupManager manager;
+
+//   PartialMemberListGroupManager({required super.id, required this.manager});
+// }
+
+// class MemberListGroup extends PartialMemberListGroupManager {
+//   final String name;
+//   final List<Member> members;
+
+//   MemberListGroup(
+//       {required this.name, required this.members, required super.id});
+// }
